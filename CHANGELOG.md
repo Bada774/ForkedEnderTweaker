abf9bfe1a5fec3e668bcdb9117dd630884c6b1c0
Bada774 <vladislavy.karelin@gmail.com>
2026-03-09 07:30:49 +0300

    rfg 2.0.2 migration + publishing changes

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## 2.0.2
+- Migrated build tooling from ForgeGradle 2.3 to RetroFuturaGradle 2.0.2
+- Fixed tank double output add bug
+- Fixed CombustionGenRecipes error message
+- Fixed Vat & SliceNSplice recipe conflict check
+- ...Minor code design changes...
+
+## 2.0.1
+- Initial public release
+- Updated compatibility with EnderIO 5.3.72
--- src/main/java/bada774/fet/EnderTweaker.java
+++ src/main/java/bada774/fet/EnderTweaker.java
@@ -4,18 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bada774.fet.utils.Logging;
+import bada774.fet.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = EnderTweaker.MODID, name = EnderTweaker.MODNAME, version = EnderTweaker.VERSION, dependencies = "required:enderio@[5.3.67,);required-after:crafttweaker;after:zenrecipereloading")
+@Mod(modid = Tags.MODID, name = Tags.MODNAME, version = Tags.VERSION, dependencies = "required:enderio@[5.3.67,);required-after:crafttweaker;after:zenrecipereloading")
 public class EnderTweaker {
-
-	public static final String MODID = "endertweaker";
-	public static final String MODNAME = "ForkedEnderTweaker";
-	public static final String VERSION = "2.0.2";
 
 	public static final List<Runnable> LATE_QUEUE = new ArrayList<>();
 	public static boolean LOAD_COMPLETE = false;
